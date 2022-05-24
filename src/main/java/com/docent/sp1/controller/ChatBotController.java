88c78a107649e40d83dcb45dfbf9bab1ebb7d9a2
SH <equation212@gmail.com>
2022-05-24 18:09:14 +0900

    0524

--- src/main/java/com/docent/sp1/controller/ChatBotController.java
+++ src/main/java/com/docent/sp1/controller/ChatBotController.java
@@ -7,21 +7,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Controller
 public class ChatBotController {
 
-    @RequestMapping(value = "/bot", method = {RequestMethod.GET, RequestMethod.POST})
-    public @ResponseBody
+    @RequestMapping(value = "/bot", method = {RequestMethod.GET, RequestMethod.POST},produces = "applicaion/json;charset=UTF-8")
+    public @ResponseBody String webhook(HttpServletRequest request, @RequestBody Map<String, Object> map) {
 
-    String webhook(HttpServletRequest request, @RequestBody Map<String, Object> map) {
         System.out.println(map);
+        String value =((Map)map.get("queryResult")).get("queryText").toString();
+        System.out.println("INPUT VALUE" + value);
+        String str ="{\"fulfillmentMessages\": [{\"text\": { \"text\": [\""+ new String("문의 접수가 완료되었습니다.")+"\"]}}]}";
 
-        String jsonObject ="";
-        return jsonObject;
+        return str;
     }
 
 
